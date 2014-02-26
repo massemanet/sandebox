0c15ded568c2a90700395a07785a68403ac26db1
mats cronqvist <masse@klarna.com>
2014-02-26 18:37:56 +0100

    refactor sandebox.js to prepare for proper linting

--- priv/static/sandebox.js
+++ priv/static/sandebox.js
@@ -6,9 +6,21 @@
 function sandebox() {
   "use strict";
 
-  function erlang_linter(cm,updater) {
-    req(cm);
-    updater(cm,{});
+  function xhr_handler(cm,updater,xhr_reply) {
+    if (updater) {
+      updater(cm,{});
+    }
+    document.getElementById("result").innerText = xhr_reply.srcElement.response;
+  }
+
+  function xhr_request(cm,updater) {
+    var xhr = new XMLHttpRequest();
+    var run = document.getElementById("run").value;
+    var code = cm.getValue();
+    xhr.open("POST", "code", true);
+    xhr.onloadend = function(xhr_reply) { xhr_handler(cm,updater,xhr_reply); };
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("code="+encodeURIComponent(code)+"&run="+encodeURIComponent(run));
   }
 
   var editor = CodeMirror.fromTextArea(
@@ -19,25 +31,11 @@ function sandebox() {
                   matchBrackets: true,
                   extraKeys: {"Tab":  "indentAuto"},
                   lint: {async: true,
-                         getAnnotations: erlang_linter,
+                         getAnnotations: xhr_request,
                          delay: 2000}
                  });
 
-  function replyhandler(oEvent) {
-    document.getElementById("result").innerText = oEvent.srcElement.response;
-  }
-
-  function req(cm) {
-    var xhr = new XMLHttpRequest();
-    var run = document.getElementById("run").value;
-    var code = cm.getValue();
-    xhr.open("POST", "code", true);
-    xhr.onloadend = replyhandler;
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send("code="+encodeURIComponent(code)+"&run="+encodeURIComponent(run));
-  }
-
-  var clik = function() { req(editor); };
+  var clik = function() { xhr_request(editor); };
   document.getElementById("eval_button").addEventListener("click",clik,false);
 
 }
